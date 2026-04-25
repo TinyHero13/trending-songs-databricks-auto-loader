b297771e2fb5ec2c5859d0e7f1cf975c581b9d2c
Yasmim Abrahão <yasmimcharles@protonmail.com>
2026-04-25 08:59:58 -0300

    feat: optimize code to make only one request

--- src/extract_songs.py
+++ src/extract_songs.py
@@ -2,18 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+from io import StringIO
 from datetime import datetime
+
+URL = "https://kworb.net/ww/"
 
 def scrape_kworb():
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get("https://kworb.net/ww/", headers=headers)
+    response = requests.get(URL, headers=headers, timeout=20)
+    response.raise_for_status()
     soup = BeautifulSoup(response.text, "html.parser")
     title = soup.find("span", class_="pagetitle").text.split(" - ")[1].split(" | ")[0].replace("/","-")
     
     match = re.search(r"(\d{4}/\d{2}/\d{2})", title)
     chart_date = match.group(1).replace("/", "-") if match else datetime.now().strftime("%Y-%m-%d")
     
-    df = pd.read_html("https://kworb.net/ww/")[0]
+    df = pd.read_html(StringIO(response.text))[0]
         
     df["chart_date"] = chart_date
     df["loaded_at"] = datetime.now().isoformat()
