81c0776d53c615bf3fc3b16cd93ad625e474d37b
TinyHero13 <yasmimcharles@protonmail.com>
2026-04-25 20:48:53 +0000

    feat: add bronze transformation

---
+++ src/songs_transaformation/transformations/bronze_layer.sql
@@ -0,0 +1,9 @@
+CREATE OR REFRESH STREAMING TABLE bronze_trending_songs
+AS SELECT
+  CAST('Pos' AS INT) As SongPosition
+  , 'P+' AS PositionChange
+  , TRIM(SPLIT('Artist and Title', '-')[0])  AS Artist
+  , TRIM(SPLIT('Artist and Title', '-')[1])  AS Title
+  , CAST(Days AS INT) as DaysPosition
+  , current_timestamp() AS IngestedAt
+FROM STREAM(songs_trending.raw.raw_trending_songs)
